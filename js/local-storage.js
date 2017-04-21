722dde354af91e98ddf490c9dc145ef0ff3056ab
mattdittmann <matt@doreydesigngroup.com>
2017-04-21 15:13:48 -0700

    add check to see if valus are in local storage and puts them in variables

--- js/local-storage.js
+++ js/local-storage.js
@@ -1,7 +1,11 @@
 if (window.localstorage) {
+  // SET UP VARIABLES TO CAPTURE FORM INPUTS
   var txtUsername = document.getElementById('username');
   var txtAnswer = document.getElementById('answer');
 
+  // CHECK TO SEE IF VALUES ARE IN LOCAL STORAGE AND PUTS THEM IN VARIABLES
   txtUsername.value = localStorage.getItem('username');
   txtAnswer.value = localStorage.getItem('answer');
+
+
 }
